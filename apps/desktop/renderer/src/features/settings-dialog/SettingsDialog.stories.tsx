b431575a85a867f29435515468963c8739dd7b55
Leeky1017 <3118895255@qq.com>
2026-02-03 13:52:19 +0800

    fix: remove unused React import from stories (#122)

--- apps/desktop/renderer/src/features/settings-dialog/SettingsDialog.stories.tsx
+++ apps/desktop/renderer/src/features/settings-dialog/SettingsDialog.stories.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import { SettingsDialog } from "./SettingsDialog";
 import { defaultGeneralSettings } from "./SettingsGeneral";
