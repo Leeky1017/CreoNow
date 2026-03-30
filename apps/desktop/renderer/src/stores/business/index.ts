9e8e403660a623761fa28b777ceda5a38ce9ac99
Leeky <3118895255@qq.com>
2026-03-31 00:31:27 +0800

    chore(renderer): clear all renderer contents

--- apps/desktop/renderer/src/stores/business/index.ts
+++
@@ -1,6 +0,0 @@
-export { useDocumentStore } from './documentStore';
-export { useProjectStore } from './projectStore';
-export { useAIStore } from './aiStore';
-export { useCharacterStore } from './characterStore';
-export { useKnowledgeGraphStore } from './knowledgeGraphStore';
-export { useSettingsStore } from './settingsStore';
